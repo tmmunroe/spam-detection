31e2fa28e95a7015081ce506e0e4a29d4acc0113
Turner Mandeville <t.m.munroe@gmail.com>
2022-11-22 17:41:25 -0500

    initial commit for spam detection

---
+++ spam_detection/lambda/spam_classifier.py
@@ -0,0 +1,64 @@
+import boto3
+from botocore.exceptions import ClientError
+from collections import namedtuple
+import os
+
+Prediction = namedtuple("Prediction", ("label", "confidence"))
+
+sender = os.getenv("SENDER_EMAIL")
+region = os.getenv("REGION")
+charset = "utf-8"
+
+ses_client = boto3.client("ses", region_name=region)
+
+
+def send_response(email, prediction:Prediction):
+    recipient = "tmm2169@columbia.edu"
+    receive_date = None
+    subject = None
+    email_body = "abcdefghijklmnopqrstuvwxyz"
+    body_sample = email_body[:240]
+
+    response_lines = "\n\n".join([
+            f"We received your email sent at {receive_date} with the subject {subject}.",
+            f"Here is a 240 character sample of the email body: {body_sample}",
+            f"The email was categorized as {prediction.label} with a {prediction.confidence}% confidence."
+        ])
+
+    try:
+        response = ses_client.send_email(
+            Source=sender,
+            Destination={
+                "ToAddresses": [recipient],
+            },
+            Message={
+                "Body": {
+                    "Text": {
+                        "Charset": charset,
+                        "Data": response_lines,
+                    },
+                },
+                "Subject": {
+                    "Charset": charset,
+                    "Data": "Email Classification Result",
+                },
+            }
+        )	
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        print("Email sent! Message ID:"),
+        print(response["MessageId"])
+
+
+def predict(email):
+    return Prediction("spam", 0.9)
+
+
+def lambda_handler(event, context):
+    email = "something"
+
+    prediction = predict(email)
+    send_response(email, prediction)
+
+    return {"status": "ok"}
--- spam_detection/spam_detection_stack.py
+++ spam_detection/spam_detection_stack.py
@@ -1,19 +1,74 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
+    Environment,
+    RemovalPolicy,
     Stack,
     # aws_sqs as sqs,
+    aws_lambda as lambda_,
+    aws_iam as iam,
+    aws_route53 as route53,
+    aws_s3 as s3,
+    aws_s3_notifications as s3n,
+    aws_ses as ses,
+    aws_ses_actions as ses_actions
 )
 from constructs import Construct
 
 class SpamDetectionStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, construct_id: str, env:Environment) -> None:
+        super().__init__(scope, construct_id, env=env)
 
-        # The code that defines your stack goes here
+        domain_name = "project-tmm2169.com"
+        hosted_zone_id = "Z03027081WT8OA0QAB8NK"
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SpamDetectionQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        email_address = f"spamDetector@{domain_name}"
+        
+        # lambda
+        lambda_classifier = lambda_.Function(self, "SpamClassifier",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            handler="spam_classifier.lambda_handler",
+            code=lambda_.Code.from_asset("spam_detection/lambda/"),
+            timeout=Duration.seconds(10),
+            environment={
+                "SENDER_EMAIL": email_address,
+                "REGION": env.region,
+            })
+
+        # s3 bucket
+        spam_detection_bucket = s3.Bucket(self, "SpamDetectionBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            versioned=True)
+        
+        spam_detection_bucket.add_object_created_notification(
+            dest=s3n.LambdaDestination(lambda_classifier)
+        )
+        
+        # SES receipt rule + S3 action
+        receipt_rules = ses.ReceiptRuleSet(self, "EmailReceiptRules")
+        receipt_rule = receipt_rules.add_rule("Email")
+        receipt_rule.add_action(
+            ses_actions.S3(
+                bucket=spam_detection_bucket,
+                object_key_prefix="emails/"
+            )
+        )
+        
+        # hosted zone
+        hosted_zone = route53.HostedZone.from_hosted_zone_attributes(self, "route53-hosted-zone", 
+            hosted_zone_id=hosted_zone_id,
+            zone_name=domain_name)
+
+        # email identity
+        email = ses.EmailIdentity(self, "EmailIdentity",
+            identity=ses.Identity.public_hosted_zone(hosted_zone=hosted_zone)
+        )
+
+        # permissions - s3 can invoke lambda, lambda can send emails
+        lambda_classifier.grant_invoke(iam.ServicePrincipal("s3.amazonaws.com"))
+
+        ses_policy = iam.PolicyStatement()
+        ses_policy.add_actions("ses:SendEmail", "ses:SendRawEmail")
+        ses_policy.add_all_resources()
+        lambda_classifier.add_to_role_policy(ses_policy)
